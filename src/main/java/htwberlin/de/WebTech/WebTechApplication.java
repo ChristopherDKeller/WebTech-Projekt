91caf8a1973af1cdb1c89d33aa44c593b252647f
ChristopherDKeller <Christopher.Keller@Student.HTW-Berlin.de>
2023-04-11 12:38:16 +0200

    Hello World

--- src/main/java/htwberlin/de/WebTech/WebTechApplication.java
+++ src/main/java/htwberlin/de/WebTech/WebTechApplication.java
@@ -2,12 +2,22 @@ package htwberlin.de.WebTech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WebTechApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebTechApplication.class, args);
+	}
+
+	@RestController
+	public class HelloWorldController {
+		@GetMapping("/helloWorld")
+		public String helloWorld(){
+			return "Hello World";
+		}
 	}
 
 }
