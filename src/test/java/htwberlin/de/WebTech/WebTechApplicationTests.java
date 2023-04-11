d7fe6c702643b496adb39acb97a45e7da37fbba4
danielstorchHTW <Daniel.Storch@student.htw-berlin.de>
2023-04-11 12:19:32 +0200

    spring

---
+++ src/test/java/htwberlin/de/WebTech/WebTechApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.de.WebTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
