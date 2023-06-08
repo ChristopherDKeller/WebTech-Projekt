da0fdd5758d9f580b593909ee0a7692916ba75a3
ChristopherDKeller <Christopher.Keller@Student.HTW-Berlin.de>
2023-06-08 14:55:23 +0200

    Meilenstein 4 fertig

--- src/main/java/htwberlin/de/WebTech/Webconfig.java
+++ src/main/java/htwberlin/de/WebTech/Webconfig.java
@@ -1,4 +1,4 @@
-package com.example.WebTech.Projekt;
+package htwberlin.de.WebTech;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
