92aee22e3268c8d1cf523fd9582f71141c7054a8
ChristopherDKeller <Christopher.Keller@Student.HTW-Berlin.de>
2023-06-27 13:29:31 +0200

    test fix

--- src/test/java/htwberlin/de/WebTech/CourseDeleteTest.java
+++ src/test/java/htwberlin/de/WebTech/CourseDeleteTest.java
@@ -1,6 +1,4 @@
 package htwberlin.de.WebTech;
-
-import org.h2.util.json.JSONValidationTargetWithoutUniqueKeys;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
--- src/test/java/htwberlin/de/WebTech/GradeDeleteTest.java
+++ src/test/java/htwberlin/de/WebTech/GradeDeleteTest.java
@@ -1,6 +1,5 @@
 package htwberlin.de.WebTech;
 
-import org.h2.util.json.JSONValidationTargetWithoutUniqueKeys;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
